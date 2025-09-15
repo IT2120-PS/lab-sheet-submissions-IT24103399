461bbcb90c95d45336287035fe11a0699d7724f6
IT24103399 <it24103399@my.sliit.lk>
2025-09-15 23:20:45 +0530

    Labsheet_06

---
+++ Lab Sheet 06/IT24103399_lab6.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\MCS\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24103399_Lab_6")
+
+#Here,random variable X has binomial distribution with n=50 and p=0.85
+
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+
+#number of calls received in one hour
+#Here,random variable Xhas poisson distribution with lambda=12
+
+dpois(15,12)
+
